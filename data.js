c2172fac3d539b3ffc1f3558a3b65047bcdd84f5
luciafonsecadesign <luciafonsecadesign@gmail.com>
2022-12-22 13:44:47 -0500

    Modificacion

--- data.js
+++ data.js
@@ -30,28 +30,72 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.034916351282666014,
-          "pitch": 0.34444677192387196,
-          "rotation": 11.780972450961727,
-          "target": "3-cama"
+          "yaw": -0.08229996304742393,
+          "pitch": 0.28597051491197156,
+          "rotation": 6.283185307179586,
+          "target": "1-cama"
         },
         {
-          "yaw": -0.7928313976717796,
-          "pitch": 0.26157199185410107,
+          "yaw": -0.8365415362056101,
+          "pitch": 0.2683124249507145,
           "rotation": 5.497787143782138,
-          "target": "1-bao"
+          "target": "2-bao"
         },
         {
-          "yaw": 0.6367515190099979,
-          "pitch": 0.1871138152516636,
-          "rotation": 7.0685834705770345,
-          "target": "2-cocina"
+          "yaw": 0.6698675936464227,
+          "pitch": 0.1567608469178534,
+          "rotation": 1.5707963267948966,
+          "target": "3-cocina"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "1-bao",
+      "id": "1-cama",
+      "name": "Cama",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.5114601530426022,
+        "pitch": 0.611681460642501,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.08343797902129424,
+          "pitch": 0.1070449948822052,
+          "rotation": 0.7853981633974483,
+          "target": "0-entrada"
+        },
+        {
+          "yaw": -1.2158592026244097,
+          "pitch": 0.1438243890322788,
+          "rotation": 0,
+          "target": "3-cocina"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-bao",
       "name": "Baño",
       "levels": [
         {
@@ -74,14 +118,14 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": 1.0762633152476013,
-        "pitch": 0.2451756503649687,
-        "fov": 1.4134061960355204
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
       },
       "linkHotspots": [
         {
-          "yaw": -1.4070138083329358,
-          "pitch": 0.24149561843128353,
+          "yaw": -1.3980296795558402,
+          "pitch": 0.17731224988594363,
           "rotation": 0,
           "target": "0-entrada"
         }
@@ -89,7 +133,7 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "2-cocina",
+      "id": "3-cocina",
       "name": "Cocina",
       "levels": [
         {
@@ -118,66 +162,22 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 1.4898058738662225,
-          "pitch": 0.12463246503917169,
+          "yaw": 1.4561097260570586,
+          "pitch": 0.07649616371425161,
           "rotation": 5.497787143782138,
           "target": "0-entrada"
         },
         {
-          "yaw": 2.6373137250954226,
-          "pitch": 0.38640559908752614,
-          "rotation": 0.7853981633974483,
-          "target": "3-cama"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "3-cama",
-      "name": "Cama",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": -1.2595661352191136,
-          "pitch": 0.19070093902710106,
+          "yaw": 2.6816722636583776,
+          "pitch": 0.37901588090646854,
           "rotation": 0,
-          "target": "2-cocina"
-        },
-        {
-          "yaw": -0.09937111458274472,
-          "pitch": 0.17822263281552608,
-          "rotation": 0.7853981633974483,
-          "target": "0-entrada"
+          "target": "1-cama"
         }
       ],
       "infoHotspots": []
     }
   ],
-  "name": "Apartamento L",
+  "name": "LIVING 35 Apartamento L",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": false,
